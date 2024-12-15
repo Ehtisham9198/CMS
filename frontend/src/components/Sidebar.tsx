daabd66375ef65aeeddaf5b0548c22fd88188e77
Brijendra-Singh2003 <brijendra0369@gmail.com>
2024-12-15 22:26:27 +0530

    table added dashboard

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -34,7 +34,7 @@ export default function Sidebar() {
 
   return (
     <div className="lg:grid grid-cols-5 xl:grid-cols-6 min-h-screen">
-      <div className="bg-white w-full shadow h-full border-r">
+      <div className="bg-white w-full h-full border-r">
         <div className="text-xl h-12 m-1 lg:m-2 relative font-bold flex gap-4 items-center">
           <img
             className="h-full aspect-square object-contain"
@@ -67,7 +67,7 @@ export default function Sidebar() {
 
         {/* DASHBOARD */}
         {/* <h5 className="hidden lg:block text-lg p-2 font-bold">Dashboard</h5> */}
-        <ul className="grid grid-cols-4 lg:flex lg:flex-col gap-1 p-2 lg:py-0 w-full lg:w-auto justify-around">
+        <ul className="grid grid-cols-4 lg:flex lg:flex-col gap-1 p-2 lg:py-0 w-full lg:w-auto justify-around border lg:border-none">
           <SidebarLink href="/dashboard">
             <RiDashboardFill size={20} />
             <span className="text-xs lg:text-base">Dashboard</span>
@@ -90,7 +90,7 @@ export default function Sidebar() {
         </ul>
       </div>
 
-      <main className="col-span-4 xl:col-span-5 bg-zinc-100">
+      <main className="col-span-4 xl:col-span-5 bg-gray-50">
         <Outlet />
       </main>
     </div>
@@ -108,7 +108,7 @@ const SidebarLink = ({
     <NavLink
       className={({ isActive }) =>
         [
-          "flex font-semibold lg:flex-row p-2 lg:border flex-col lg:gap-2 items-center lg:px-5 lg:py-3 rounded ",
+          "flex font-semibold lg:flex-row lg:p-2 lg:border flex-col lg:gap-2 items-center lg:px-5 lg:py-3 rounded ",
           isActive
             ? "text-black border-black"
             : "text-black/60 hover:border-rail-dark",
--- frontend/src/hooks/requests.ts
+++ frontend/src/hooks/requests.ts
@@ -1,4 +1,6 @@
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000";
+import { IFile } from "../pages/Dashboard";
+
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000";
 
 export async function logout(): Promise<string | null> {
     try {
@@ -43,3 +45,25 @@ export async function login(username: string, password: string): Promise<string 
         return "something went wrong";
     }
 }
+
+export async function getFiles(): Promise<IFile[]> {
+    return [
+        { id: "cse/45", title: "Admission", created_by: "Rakesh", forwarded_by: "birju_op", created_at: "24 dec 2025" },
+        { id: "cse/22", title: "AK dash", created_by: "Ajaya", forwarded_by: "birju_op", created_at: "24 dec 2025" },
+        { id: "ce/20", title: "STudents", created_by: "Pradyut", forwarded_by: "birju_op", created_at: "24 dec 2025" },
+        { id: "it/69", title: "STudents", created_by: "Pradyut", forwarded_by: "birju_op", created_at: "24 dec 2025" },
+    ]
+}
+
+// const getFiles = async () => {
+//     try {
+//         const response = await fetch("http://localhost:3000/api/recievedFile", {
+//             credentials: "include",
+//         });
+//         const data = await response.json();
+//         console.log(data);
+//         setFiles(data.fileData);
+//     } catch (error) {
+//         console.error("Error fetching files:", error);
+//     }
+// };
